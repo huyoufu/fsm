c2ae91f094b178c723c1e0e438ea6c0fd19b8284
huyoufu <huyoufu19888@163.com>
2016-04-10 01:16:52 +0800

    添加两个属性 服务器地址 和端口号

--- src/main/java/com/gis09/fsm/common/config/FSMConfig.java
+++ src/main/java/com/gis09/fsm/common/config/FSMConfig.java
@@ -9,22 +9,39 @@ public class FSMConfig {
 	public static final long FSM_HEART_INTERVAL = 45; // 默认的心跳间隔是45秒
 	public static final long FSM_RECONNECT_INTERVAL = 60;// 重连间隔
 	public static final long FSM_RECONNECT_NUM = 5;// 默认的重试次数 重试五次放弃
+	public static final String FSM_DEFAULT_SERVER_HOST="127.0.0.1"; //默认绑定的地址
+	public static final long FSM_DEFAULT_PORT=52018;//默认的启动端口
 	private long fsm_heart_interval=FSM_HEART_INTERVAL;
 	
 	private long fsm_reconnect_interval=FSM_RECONNECT_INTERVAL;
 	
 	private long fsm_reconnect_num=FSM_RECONNECT_NUM;
-
+	
+	private String fsm_server_host=FSM_DEFAULT_SERVER_HOST;
+	
+	private long fsm_port=FSM_DEFAULT_PORT;
 	public long getFsm_heart_interval() {
 		return fsm_heart_interval;
 	}
-
-	public FSMConfig(long fsm_heart_interval, long fsm_reconnect_interval, long fsm_reconnect_num) {
+	
+	
+	public FSMConfig(long fsm_heart_interval, long fsm_reconnect_interval,
+			long fsm_reconnect_num, String fsm_server_host, long fsm_port) {
 		super();
 		this.fsm_heart_interval = fsm_heart_interval;
 		this.fsm_reconnect_interval = fsm_reconnect_interval;
 		this.fsm_reconnect_num = fsm_reconnect_num;
+		this.fsm_server_host = fsm_server_host;
+		this.fsm_port = fsm_port;
 	}
+	
+	public FSMConfig(String fsm_server_host, long fsm_port) {
+		super();
+		this.fsm_server_host = fsm_server_host;
+		this.fsm_port = fsm_port;
+	}
+
+
 	public FSMConfig() {
 		super();
 	}
@@ -49,4 +66,30 @@ public class FSMConfig {
 		this.fsm_reconnect_num = fsm_reconnect_num;
 	}
 
+	public String getFsm_server_host() {
+		return fsm_server_host;
+	}
+
+	public void setFsm_server_host(String fsm_server_host) {
+		this.fsm_server_host = fsm_server_host;
+	}
+
+	public long getFsm_port() {
+		return fsm_port;
+	}
+
+	public void setFsm_port(long fsm_port) {
+		this.fsm_port = fsm_port;
+	}
+
+
+	@Override
+	public String toString() {
+		return "FSMConfig [fsm_heart_interval=" + fsm_heart_interval
+				+ ", fsm_reconnect_interval=" + fsm_reconnect_interval
+				+ ", fsm_reconnect_num=" + fsm_reconnect_num
+				+ ", fsm_server_host=" + fsm_server_host + ", fsm_port="
+				+ fsm_port + "]";
+	}
+	
 }
